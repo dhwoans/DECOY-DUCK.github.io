e96f9aa015e579170f8c8a8ac90bab814fdef0ae
tph01198@naver.com <Stph01198@naver.com6>
2023-02-28 23:00:50 +0900

    초안 추가

---
+++ _drafts/밑바닥부터_만드는_컴퓨팅_시스템/04/fill/Fill.asm
@@ -0,0 +1,58 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+// Put your code here.
+@color         // declare color variable
+M=0            // by default is white
+
+(loop)
+    @SCREEN    // RAM address 16384
+    D=A
+
+    @pixels 
+    M=D       // pixel address (starting point: 16384, max: 16384+8192=24576)
+
+    @KBD      // D = ascii code of a keyboard input
+    D=M
+
+    @black
+    D;JGT     // if(keyboard > 0) goto black
+
+    @color
+    M=0       // otherwise white
+    
+    @color_screen
+    0;JMP     // jump to subroutine that actually changes the color of screen
+
+    (black)
+        @color
+        M=-1  // set to black
+
+    (color_screen)
+        @color
+        D=M
+        @pixels
+        A=M
+        M=D   // M[pixels] = @color
+
+        @pixels
+        M=M+1
+        D=M
+
+        @24576 // loop until end of pixels
+        D=D-A
+        @color_screen
+        D;JLT
+
+@loop
+0;JMP         // infinite loop
